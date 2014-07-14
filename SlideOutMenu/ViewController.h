46856a628a2a91ec8a12e84587b970f46d425987
Jared Davidson <Davidson@Jareds-iMac.local>
2014-07-14 14:05:54 -0600

    Initial Commit

---
+++ SlideOutMenu/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SlideOutMenu
+//
+//  Created by Jared Davidson on 7/14/14.
+//  Copyright (c) 2014 Archetapp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
